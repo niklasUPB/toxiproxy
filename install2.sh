0ea6b34b2e31ba6d2a1913f2d2e8f434b53d55ae
niklas <105489239+niklasUPB@users.noreply.github.com>
2022-10-15 19:01:10 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,3 +1,3 @@
-$ wget -O toxiproxy-2.1.4.deb https://github.com/Shopify/toxiproxy/releases/download/v2.1.4/toxiproxy_2.1.4_amd64.deb
-$ sudo dpkg -i toxiproxy-2.1.4.deb
-$ sudo service toxiproxy start
+wget -O toxiproxy-2.1.4.deb https://github.com/Shopify/toxiproxy/releases/download/v2.1.4/toxiproxy_2.1.4_amd64.deb
+sudo dpkg -i toxiproxy-2.1.4.deb
+sudo service toxiproxy start
