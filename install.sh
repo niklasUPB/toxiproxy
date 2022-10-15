92fab693588f032261c62403ae58507e7366c41e
niklas <105489239+niklasUPB@users.noreply.github.com>
2022-10-15 18:40:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 sudo apt-get remove golang-go
 sudo apt-get remove --auto-remove golang-go
 sudo rm -rvf /usr/local/go
-wget https://dl.google.com/go/go1.17.linux-amd64.tar.gz
 sudo tar -xvf go1.17.linux-amd64.tar.gz
 sudo mv go /usr/local
 export GOROOT=/usr/local/go
